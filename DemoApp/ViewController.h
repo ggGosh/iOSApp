d046f0394762e264c12edbb885fca9bfd131cc4b
谭学勇 <tanxueyong@tanxueyongdeMacBook-Air.local>
2017-10-26 17:17:43 +0800

    Initial Commit

---
+++ DemoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoApp
+//
+//  Created by 谭学勇 on 2017/10/26.
+//  Copyright © 2017年 谭学勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoApp
+//
+//  Created by 谭学勇 on 2017/10/26.
+//  Copyright © 2017年 谭学勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
