b2de34c30ee3dd02bc84de595efb96250bf111a4
BH <BH@baodeMacBook-Air.local>
2017-11-02 17:27:29 +0800

    Test test!!

--- DemoApp/ViewController.m
+++ DemoApp/ViewController.m
@@ -30,8 +30,9 @@ static ViewController *viewController = nil;
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+//    [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
     self.navigationBarHidden = YES;
     self.view.backgroundColor = [UIColor whiteColor];
     viewController=self;
